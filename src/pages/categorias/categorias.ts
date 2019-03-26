ebb906e91fc58891cc0a933cf98a2c16ee481a52
FHOCFHOC <backintheusa@gmail.com>
2019-03-26 14:08:57 -0300

    mostrando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -16,17 +18,21 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
+  bucketUrl: string = API_CONFIG.bucketBaseUrl;
+
+  items: CategoriaDTO[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams, public categoriaService: CategoriaService) {
   }
 
   ionViewDidLoad() {
     this.categoriaService.findAll()
-      .subscribe(response => {
-        console.log(response);
-      },
-      error => {
-        console.log(error);
-      });
+    .subscribe(response => {
+      this.items = response;
+    },
+    error => {
+      console.log(error);
+    });
   }
 
 }
